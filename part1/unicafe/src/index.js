012c0cf94ed4ef56fde48539071b3136d20bc58a
Akash Pandey <akskipadhai@gmail.com>
2019-08-05 01:21:16 +0530

    1.8: unicafe step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,31 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = (props) => {
+    
+  const getAverage = (props) => {
+      const wtSum = 1*props.good + 0*props.neutral - 1*props.bad
+      const denom = ((props.good+props.neutral+props.bad)==0)? 1:props.good+props.neutral+props.bad
+      return wtSum/denom
+  }
+
+  const getPositive = (props) => {
+     const denom = ((props.good+props.neutral+props.bad)==0)? 1:props.good+props.neutral+props.bad
+     return props.good/denom * 100
+  }
+  return (
+   <div>
+    <h1>statistics</h1>
+    <p>good {props.good}</p>
+    <p>neutral {props.neutral}</p>
+    <p>bad {props.bad}</p>
+    <p>all {props.good+props.neutral+props.bad}</p>
+    <p>average {getAverage(props)}</p>
+    <p>positive {getPositive(props)}%</p>
+   </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -11,31 +36,13 @@ const App = () => {
   const handleNeutral = () => (setNeutral(neutral+1))
   const handleBad = () => (setBad(bad+1))
 
-  const getAverage = () => {
-      const wtSum = 1*good + 0*neutral - 1*bad
-      const denom = ((good+neutral+bad)==0)? 1:good+neutral+bad
-      return wtSum/denom
-  }
-  
-
-  const getPositive = () => {
-     const denom = ((good+neutral+bad)==0)? 1:good+neutral+bad
-     return good/denom * 100
-  }
-
   return (
     <div>
-      <h2>give feedback</h2> 
-      <button onClick={() => handleGood()}>good</button>
-      <button onClick={() => handleNeutral()}>neutral</button>
-      <button onClick={() => handleBad()}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral} </p>
-      <p>bad {bad}</p>
-      <p>all {good+neutral+bad}</p>
-      <p>average {getAverage()}</p>
-      <p>positive {getPositive()}%</p>
+    <h2>give feedback</h2> 
+    <button onClick={() => handleGood()}>good</button>
+    <button onClick={() => handleNeutral()}>neutral</button>
+    <button onClick={() => handleBad()}>bad</button>
+    <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
